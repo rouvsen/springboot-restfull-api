1e16442e3afc92bddce5a1274d09ed31eb7d77df
rovshan <rouvsen@gmail.com>
2023-04-17 22:09:33 +0400

    main class - added

---
+++ src/main/java/com/rouvsen/SpringBootPractice2Application.java
@@ -0,0 +1,13 @@
+package com.rouvsen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPractice2Application.class, args);
+	}
+
+}
